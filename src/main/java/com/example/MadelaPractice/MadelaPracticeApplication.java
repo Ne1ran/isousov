158502c2d6b3ecccb742e9d1eddf57d28378226f
Neiran <pepega@gmail.com>
2022-06-09 11:24:54 +0300

    Added starting project for practice work in Madela

---
+++ src/main/java/com/example/MadelaPractice/MadelaPracticeApplication.java
@@ -0,0 +1,18 @@
+package com.example.MadelaPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class MadelaPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadelaPracticeApplication.class, args);
+
+		System.out.println("Check");
+	}
+
+
+
+}
